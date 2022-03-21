e39bab885fffce98842c08c99c7ae02b1e4e8c7d
yangstd <yangstd@atFingerprint.com>
2022-03-21 17:23:36 +0800

    master

---
+++ build/CMakeFiles/test_nano_line.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_nano_line.dir/nano_line.cpp.o"
+  "CMakeFiles/test_nano_line.dir/test_nano_line.cpp.o"
+  "test_nano_line.pdb"
+  "test_nano_line"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_nano_line.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/test_nano_line.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for test_nano_line.
+# This may be replaced when dependencies are built.
